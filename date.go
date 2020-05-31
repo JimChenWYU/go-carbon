56766a1bdf71ecca35d48fda913f4f4c906a72d2
JimChen <imjimchen@163.com>
2020-05-31 10:10:53 +0800

    add note
    Signed-off-by: JimChen <imjimchen@163.com>

--- date.go
+++ date.go
@@ -18,113 +18,131 @@ func (c *Carbon) Timezone() string {
 	return c.Location().String()
 }
 
+// Add seconds to the instance.
 func (c *Carbon) AddSeconds(s int) *Carbon {
 	return NewCarbon(c.Add(time.Duration(s) * time.Second))
 }
 
+// Add minutes to the instance.
 func (c *Carbon) AddMinutes(m int) *Carbon {
 	return NewCarbon(c.Add(time.Duration(m) * time.Minute))
 }
 
+// Add hours to the instance.
 func (c *Carbon) AddHours(h int) *Carbon {
 	return NewCarbon(c.Add(time.Duration(h) * time.Hour))
 }
 
+// Add days to the instance.
 func (c *Carbon) AddDays(d int) *Carbon {
 	return NewCarbon(c.AddDate(0, 0, d))
 }
 
+// Add weeks to the instance.
 func (c *Carbon) AddWeeks(w int) *Carbon {
 	return c.AddDays(DaysPerWeek * w)
 }
 
+// Add months to the instance.
 func (c *Carbon) AddMonths(m int) *Carbon {
 	return NewCarbon(c.AddDate(0, m, 0))
 }
 
+// Add years to the instance.
 func (c *Carbon) AddYears(y int) *Carbon {
 	return NewCarbon(c.AddDate(y, 0, 0))
 }
 
+// Sub seconds to the instance.
 func (c *Carbon) SubSeconds(s int) *Carbon {
 	return c.AddSeconds(-s)
 }
 
+// Sub minutes to the instance.
 func (c *Carbon) SubMinutes(m int) *Carbon {
 	return c.AddMinutes(-m)
 }
 
+// Sub hours to the instance.
 func (c *Carbon) SubHours(h int) *Carbon {
 	return c.AddHours(-h)
 }
 
+// Sub days to the instance.
 func (c *Carbon) SubDays(d int) *Carbon {
 	return NewCarbon(c.AddDate(0, 0, -d))
 }
 
+// Sub weeks to the instance.
 func (c *Carbon) SubWeeks(w int) *Carbon {
 	return c.SubDays(DaysPerWeek * w)
 }
 
+// Sub months to the instance.
 func (c *Carbon) SubMonths(m int) *Carbon {
 	return NewCarbon(c.AddDate(0, -m, 0))
 }
 
+// Sub years to the instance.
 func (c *Carbon) SubYears(y int) *Carbon {
 	return NewCarbon(c.AddDate(-y, 0, 0))
 }
 
-/**
- * Set the date with gregorian year, month and day numbers.
- */
+// Set the date with gregorian year, month and day numbers.
 func (c *Carbon) SetDate(year int, month time.Month, day int) *Carbon {
 	c.Time = time.Date(year, month, day, c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
-/**
- * Resets the current time of the DateTime object to a different time.
- */
+// Resets the current time of the DateTime object to a different time.
 func (c *Carbon) SetTime(hour, minute, second, nanosecond int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), c.Day(), hour, minute, second, nanosecond, c.Location())
 	return c
 }
 
+// Sets the date and time based on a Unix timestamp.
 func (c *Carbon) SetTimestamp(t int64) *Carbon {
 	c.Time = time.Unix(t, 0)
 	return c
 }
 
+// Set current instance nanoseconds to the given value.
 func (c *Carbon) SetNanoSeconds(ns int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), c.Day(), c.Hour(), c.Minute(), c.Second(), ns, c.Location())
 	return c
 }
 
+// Set current instance seconds to the given value.
 func (c *Carbon) SetSeconds(s int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), c.Day(), c.Hour(), c.Minute(), s, c.Nanosecond(), c.Location())
 	return c
 }
 
+// Set current instance minutes to the given value.
 func (c *Carbon) SetMinutes(m int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), c.Day(), c.Hour(), m, c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
+// Set current instance hours to the given value.
 func (c *Carbon) SetHours(h int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), c.Day(), h, c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
+// Set current instance days to the given value.
 func (c *Carbon) SetDays(d int) *Carbon {
 	c.Time = time.Date(c.Year(), c.Month(), d, c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
+// Set current instance months to the given value.
 func (c *Carbon) SetMonths(m time.Month) *Carbon {
 	c.Time = time.Date(c.Year(), m, c.Day(), c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
 }
 
+// Set current instance years to the given value.
 func (c *Carbon) SetYears(y int) *Carbon {
 	c.Time = time.Date(y, c.Month(), c.Day(), c.Hour(), c.Minute(), c.Second(), c.Nanosecond(), c.Location())
 	return c
--- timestamp.go
+++ timestamp.go
@@ -2,7 +2,8 @@ package carbon
 
 import "time"
 
-func CreateFromTimestamp(timestamp int64, location string) (*Carbon, error)  {
+// Create a Carbon instance from a timestamp.
+func CreateFromTimestamp(timestamp int64, location string) (*Carbon, error) {
 	l, err := time.LoadLocation(location)
 	if err != nil {
 		return nil, err
@@ -13,14 +14,17 @@ func CreateFromTimestamp(timestamp int64, location string) (*Carbon, error)  {
 	return t, nil
 }
 
+// Returns the UNIX timestamp for the current date.
 func (c *Carbon) Timestamp() int64 {
 	return c.Unix()
 }
 
+// Returns the UNIX millitimestamp for the current date.
 func (c *Carbon) MilliTimestamp() int64 {
 	return c.UnixNano() / 1e6
 }
 
+// Returns the UNIX microtimestamp for the current date.
 func (c *Carbon) MicroTimestamp() int64 {
 	return c.UnixNano() / 1e3
 }
